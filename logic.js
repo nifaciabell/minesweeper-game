6bf6272889374914f8eb9d7ab1f86100532fa43e
Nifacia Bell <nifaciabell.tech@gmail.com>
2023-02-28 16:05:21 -0500

    Update

---
+++ logic.js
@@ -0,0 +1,32 @@
+// Use a function to create the board.
+
+// For loop to loop over a square in the grid 
+
+// Create a new variable using the DOM that is assigned to create 100 divs since I am looping through 100x
+
+// Set an attribute 'id' to each square to call on later 
+
+// Put squares that were just created into the grid div by using appendChild method
+
+// Create a variable set to an empty array outside of my function. 
+
+// In the global scope create a variable of bombAccount 
+
+// In the function create an array 
+
+// Push created individual square into square array --> this will create an array of 100 squares
+
+// ** The fill() method changes all elements in an array to a static value, from a start index (default 0) to an end index (default array.length). It returns the modified array.
+
+ // Create shuffled game array with random bombs
+
+ // Merge arrays so that I can be able to randomize the elements.
+
+//  const shuffleArray = mergeArrays.sort(() => 0.5 - Math.random());
+ // This method puts the elements into random order. 
+
+ //---> Now that I have an array with randomize elements I need to insert this array into my grid. Each cell created in my div needs to be either a bomb or safe cell. Insert shuffleArray into my empty squares array.
+
+ // Now that the bombs have been added to the board/grid now I need to add the numbers. 
+// In minesweeper if a cell is clicked that is a number then the bomb is adjacent to that number I need to create a code that will allow me to create a pattern so that the user will have an idea where the bomb is based on the number. 
+
